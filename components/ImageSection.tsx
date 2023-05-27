e00e980d7cf20e99c7715c31abd410df9037eff4
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2023-05-27 17:19:05 +0100

    remove hooks

--- components/ImageSection.tsx
+++ components/ImageSection.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
 const ImageSection = ({ image }) => {
   return <Image src={image} width={300} height={300} alt={image} />;
