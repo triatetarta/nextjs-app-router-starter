4a008093a35bb6e31e80749fbff535bbd90443fc
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2023-05-27 17:24:04 +0100

    remove error

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,6 @@ import ImageSection from '@/components/ImageSection';
 import Link from 'next/link';
 
 const fetchData = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  await new Promise((resolve, reject) => {
-    reject(new Error('This is wrong!'));
-  });
-
   const response = fetch(
     'https://jsonplaceholder.typicode.com/albums/1/photos'
   );
